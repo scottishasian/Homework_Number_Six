58820168abe2683ffa2b16de87aa0db364b2ce21
Kynan <k.song@blueyonder.co.uk>
2017-10-02 17:22:51 +0100

    Wrote getter methods to return team values.

---
+++ Task_B/spec/sports_team_spec.rb
@@ -0,0 +1,23 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( '../sports_team.rb' )
+
+class TestSportsTeam < MiniTest::Test
+
+  def setup
+    @Team1 = SportsTeam.new("HWUBC", ["Kynan","Hope","Stuart","Adam","Michael"], "Graeme")
+  end
+
+  def test_team_name
+    assert_equal("HWUBC", @Team1.team_name)
+  end
+
+  def test_rowers
+    assert_equal(["Kynan","Hope","Stuart","Adam","Michael"], @Team1.rowers)
+  end
+
+  def test_coach
+    assert_equal("Graeme", @Team1.coach)
+  end
+
+end
---
+++ Task_B/sports_team.rb
@@ -0,0 +1,21 @@
+class SportsTeam
+
+  def initialize(team_name, rowers, coach)
+    @team_name = team_name
+    @rowers = rowers
+    @coach = coach
+  end
+
+  def team_name
+    return @team_name
+  end
+
+  def rowers
+    return @rowers
+  end
+
+  def coach
+    return @coach
+  end
+
+end
